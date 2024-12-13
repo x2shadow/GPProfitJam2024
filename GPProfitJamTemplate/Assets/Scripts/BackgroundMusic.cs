5b777ddd2526cbcfd4450384713d57aaa416415f
X2SHADOW <hahandrew88@gmail.com>
2024-12-13 14:08:39 +0700

    Background Music Bugfix

--- GPProfitJamTemplate/Assets/Scripts/BackgroundMusic.cs
+++ GPProfitJamTemplate/Assets/Scripts/BackgroundMusic.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class BackgroundMusic : MonoBehaviour
 {
     public static BackgroundMusic Instance;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip startMenuMusic;
+    [SerializeField] private AudioClip level1Music;
 
     void Awake()
     {
-        if(Instance != null && Instance != this)
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
         }
@@ -17,6 +20,43 @@ public class BackgroundMusic : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (audioSource == null)
+            return;
+
+        // Включаем музыку для сцены StartMenu
+        if (scene.name == "StartMenu")
+        {
+            PlayMusic(startMenuMusic);
+        }
+        // Включаем музыку для Level1 и всех остальных уровней
+        else if (scene.name.StartsWith("Level"))
+        {
+            PlayMusic(level1Music);
+        }
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        if (audioSource.clip != clip)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
         }
     }
 }
