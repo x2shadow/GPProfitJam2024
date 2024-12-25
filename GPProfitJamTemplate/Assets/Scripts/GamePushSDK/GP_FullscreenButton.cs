5da6a237b1f8264a33367ad8ed879caca89e191b
X2SHADOW <hahandrew88@gmail.com>
2024-12-26 03:01:05 +0700

    StartMenu UI Scaling fix

---
+++ GPProfitJamTemplate/Assets/Scripts/GamePushSDK/GP_FullscreenButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using GamePush;
+using UnityEngine;
+
+public class GP_FullscreenButton : MonoBehaviour
+{
+    public void FullscreenToggle()
+    {
+        GP_Fullscreen.Toggle();
+    }
+}
